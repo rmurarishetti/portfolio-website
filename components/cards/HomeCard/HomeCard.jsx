c3b71cbc47c18c47251da4842ad88638f904a265
Rohit Nag <rnag2018@gmail.com>
2022-10-09 14:09:56 +0100

    Updated home page and added images

--- components/cards/HomeCard/HomeCard.jsx
+++ components/cards/HomeCard/HomeCard.jsx
@@ -3,7 +3,16 @@ import Tilt from 'react-parallax-tilt';
 
 function HomeCard({ link, title, description, image }) {
     return (
-        <Tilt tiltReverse glareEnable tiltMaxAngleX={10} tiltMaxAngleY={10} glareColor='#0070f3' glareMaxOpacity={0.5} glareBorderRadius={10} className={styles.card}>
+        <Tilt
+            tiltReverse
+            glareEnable
+            scale={1.05}
+            tiltMaxAngleX={5}
+            tiltMaxAngleY={5}
+            glareColor='var(--color-accent-primary)'
+            glareMaxOpacity={0.7}
+            glareBorderRadius={10}
+            className={styles.card}>
             <a href={link} >
                 <h2>{title} &rarr;</h2>
                 <p>{description}</p>
