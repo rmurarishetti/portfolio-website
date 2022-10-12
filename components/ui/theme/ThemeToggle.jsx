5ce67ee6197cd7cf39bb943b3ab282f862cc6381
Rohit Nag <rnag2018@gmail.com>
2022-10-12 12:05:02 +0100

    Added spinning themeToggle animation

--- components/ui/theme/ThemeToggle.jsx
+++ components/ui/theme/ThemeToggle.jsx
@@ -3,36 +3,28 @@ import styles from './ThemeToggle.module.scss'
 import { useState, useEffect } from 'react';
 
 function ThemeToggle() {
-    const [isMounted, setIsMounted] = useState(false)
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
-
-    const getSystemTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-
-    const getFormattedTheme = (curTheme) => {
-        if (curTheme == 'light' || curTheme == 'dark') {
-            return curTheme;
-        }
-        if (typeof window === "undefined") {
-            return 'dark';
-        }
-        return getSystemTheme();
-    }
-
+    const [isMounted, setIsMounted] = useState(false);
     const { theme, setTheme } = useTheme();
 
-    let themeClass = getFormattedTheme(theme) == 'dark' ? styles.dark : styles.light;
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
 
     const toggleTheme = () => {
-        setTheme(getFormattedTheme(theme) == 'dark' ? 'light' : 'dark');
+        setTheme(theme == 'dark' ? 'light' : 'dark');
     }
+
+    let themeClass = theme == 'dark' ? styles.dark : styles.light;
 
     return (
         isMounted && <div onClick={toggleTheme} className={[styles.themeToggle, themeClass].join(' ')}>
-            <div className={styles.sun}>🌞</div>
-            <div className={styles.moon}>🌚</div>
+            <div className={[styles.icon, styles.sun].join(' ')}>
+                <div>🌞</div>
+            </div>
+            <div className={[styles.icon, styles.moon].join(' ')}>
+                <div>🌚</div>
+            </div>
         </div>
     );
 }
