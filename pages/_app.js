c435bd895cc27f12452a87bb3373b02f9c0adc29
Rohit Nag <rnag2018@gmail.com>
2022-10-12 01:56:18 +0100

    (potentially) fixed theme hydration issue

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,20 @@ import '../styles/globals.scss'
 import { ThemeProvider } from 'next-themes'
 import { Navbar } from '../components/layout/navs';
 import Footer from '../components/layout/Footer';
+import { useState, useEffect } from "react"
+
 
 function MyApp({ Component, pageProps }) {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   return (
     <ThemeProvider themes={['light', 'dark']} enableSystem={true} defaultTheme={'system'} >
       <Navbar />
-      <Component {...pageProps} />
+      {isMounted && <Component {...pageProps} />}
       <Footer />
     </ThemeProvider>
   )
