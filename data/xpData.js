1c89d033622689bdda76cbe81fb1413034a93ee0
Rohit Nag <rnag2018@gmail.com>
2022-10-31 09:19:27 +0000

    Started adding xpData.

---
+++ data/xpData.js
@@ -0,0 +1,35 @@
+export const xpData = [
+    {
+        logo: "/images/logos/microsoft_logo.svg",
+        company: "Microsoft",
+        roles: [
+            {
+                position: "Software Engineer Intern",
+                startDate: new Date(2022, 7 - 1),
+                endDate: new Date(2022, 9 - 1),
+                description: "Working within the MSAI team.",
+                skills: ["C++", "C#", "XAML", "React"]
+            }
+        ]
+    },
+    {
+        logo: "/images/logos/rr_logo.svg",
+        company: "Rolls-Royce",
+        roles: [
+            {
+                position: "Systems Design Intern",
+                startDate: new Date(2022, 1 - 1),
+                endDate: new Date(2022, 7 - 1),
+                description: "Worked on the Rolls-Royce led 11-partner R&T project, COLIBRI, to improve design automation within the aerospace industry through introducing ML and AI.",
+                skills: ["C#", "Python", "MATLAB", "Siemens NX"]
+            },
+            {
+                position: "Automation & Controls Intern",
+                startDate: new Date(2021, 6 - 1),
+                endDate: new Date(2021, 12 - 1),
+                description: "Worked within the Artificial Chief Engineer (ACE) team, developing RR's new autonomous machinery control system for Naval vessels.",
+                skills: ["HTML", "JavaScript", "CSS"]
+            }
+        ]
+    }
+]
