197929b05beccec1590cb433285462988d260e92
Rohit Nag <rnag2018@gmail.com>
2022-12-08 20:16:19 +0000

    Tried adding google analytics v2

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,10 @@
 import '../styles/globals.scss'
 import { useState, useEffect } from "react"
 import { ThemeProvider } from 'next-themes'
-import { GoogleAnalytics, event } from "nextjs-google-analytics";
 import NextNProgress from 'nextjs-progressbar';
 import { Navbar, Footer } from '../components/layout'
 import { useRouter } from 'next/router'
-
-export function reportWebVitals({ id, name, label, value }) {
-  event(name, {
-    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    label: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate.
-  });
-}
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -25,7 +16,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <GoogleAnalytics trackPageViews />
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-174262112-1" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-174262112-1', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
       <ThemeProvider themes={['light', 'dark']} enableSystem={true} defaultTheme={'system'} >
         <NextNProgress color="#6F3BFF" startPosition={0.3} stopDelayMs={200} height={6} showOnShallow={true} />
         <Navbar />
