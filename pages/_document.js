2f04f12452fd0210d7e56f06c5b97259589d7a60
Rohit Nag <rnag2018@gmail.com>
2022-12-08 13:32:39 +0000

    Attempted to fix vp bug v2.

--- pages/_document.js
+++ pages/_document.js
@@ -12,9 +12,10 @@ export default function Document() {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
-                <meta name="msapplication-TileColor" content="#da532c" />
+                {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta> */}
+                {/* <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff"></meta> */}
             </Head>
             <body>
                 <Main />
