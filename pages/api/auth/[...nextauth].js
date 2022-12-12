47a256930eedfd7389783566c94b44216def3507
Rohit Nag <rnag2018@gmail.com>
2022-12-12 10:12:49 +0000

    ADded callbacks redirect to [..nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,18 @@ import NextAuth from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    secret: process.env.NEXTAUTH_SECRET
+    callbacks: {
+        redirect: async (url, baseUrl) => {
+            return url.startsWith(baseUrl)
+                ? Promise.resolve(url)
+                : Promise.resolve(baseUrl)
+        }
+    },
 });
