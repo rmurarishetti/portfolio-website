960425ad504fc6225e0da337ab3f4f128fce8324
Rohit Nag <rnag2018@gmail.com>
2023-04-05 19:06:01 +0100

    fixed swiper view bug

--- components/swipers/ThumbnailSwiper/index.jsx
+++ components/swipers/ThumbnailSwiper/index.jsx
@@ -17,9 +17,6 @@ function ThumbnailSwiper({ projectsData, activeIndex = 0 }) {
     const incrementSlide = (i) => {
         i > 0 ? swiper.slideNext() : swiper.slidePrev();
     }
-
-    const isLeftActive = activeSlide > 0;
-    const isRightActive = activeSlide < len - 1;
     return (
         <Swiper
             onSwiper={setSwiper}
@@ -28,8 +25,8 @@ function ThumbnailSwiper({ projectsData, activeIndex = 0 }) {
             slidesPerView={'auto'}
             pagination={{ clickable: true }}
             keyboard={{ enabled: true }}
+            loop={true}
             centeredSlides={true}
-            // centeredSlidesBounds={true}
             initialSlide={0}
             mousewheel
             grabCursor
@@ -40,7 +37,7 @@ function ThumbnailSwiper({ projectsData, activeIndex = 0 }) {
             <div className={styles.fade} />
             {projectsData.map((projectData, i) => {
                 return (
-                    <SwiperSlide key={projectData.id} className={styles.swiperSlide} >
+                    <SwiperSlide key={projectData.id} className={[styles.swiperSlide, i === activeSlide ? styles.active : ''].join(' ')}>
                         <ProjectCard {...projectData} />
                     </SwiperSlide>
                 )
@@ -48,8 +45,8 @@ function ThumbnailSwiper({ projectsData, activeIndex = 0 }) {
             {swiper &&
                 <div className={styles.footer}>
                     <div
-                        className={[styles.arrow, styles.leftArrow, !isLeftActive ? styles.disabled : ''].join(' ')}
-                        onClick={() => isLeftActive ? incrementSlide(-1) : null}>
+                        className={[styles.arrow, styles.leftArrow].join(' ')}
+                        onClick={() => incrementSlide(-1)}>
                         ←
                     </div>
                     <div className={styles.fraction}>
@@ -58,8 +55,8 @@ function ThumbnailSwiper({ projectsData, activeIndex = 0 }) {
                         <div className={styles.total}>{len}</div>
                     </div>
                     <div
-                        className={[styles.arrow, styles.rightArrow, !isRightActive ? styles.disabled : ''].join(' ')}
-                        onClick={() => isRightActive ? incrementSlide(1) : null}>
+                        className={[styles.arrow, styles.rightArrow].join(' ')}
+                        onClick={() => incrementSlide(1)}>
                         →
                     </div>
                 </div>
