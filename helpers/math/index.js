83aca0fbdc97cfb2c700e2ce0812e3133664cb16
Rohit Nag <rnag2018@gmail.com>
2023-06-12 07:24:32 +0100

    bug fix try 4: removed cubicBezierAtTime helper function

--- helpers/math/index.js
+++ helpers/math/index.js
@@ -1,6 +1,6 @@
 import { coordinates2cartesian } from "./coordinates2cartesian";
-import { cubicBezierCurve, cubicBezierAtTime } from "./cubicBezierCurve";
+import { cubicBezierCurve } from "./cubicBezierCurve";
 import { daysDiff } from "./dateDiff";
 import { distanceBetween } from "./trig";
 
-export { coordinates2cartesian, cubicBezierCurve, daysDiff, cubicBezierAtTime, distanceBetween };
+export { coordinates2cartesian, cubicBezierCurve, daysDiff, distanceBetween };
